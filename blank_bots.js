44d3de83a141539543674af00cc22c08a5d202e9
Mark Norwich <MarkNorwich@marks-mbp.dhcp.wustl.edu>
2015-12-14 03:01:44 -0600

    bots

--- blank_bots.js
+++ blank_bots.js
@@ -1,13 +1,14 @@
 window.botList = window.botList || [];
 
 function QuickBot() {
-    this.name = "jiri rapatti";
+    this.name = "Mark Norwich";
     this.keyAction = function(key) {w};
     this.displayText = function() {return [s];};
     this.mainLoop = function() {
         return [screenToGameX(getMouseX()),
                 screenToGameY(getMouseY())];
     };
+    console.log("first bot");
 }
 console.log("first bot");
 window.botList.push(new QuickBot());
